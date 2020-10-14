fc9a9127c8dff41e1298572692660ea294222eef
valdirbrasileiro <valdir.ramos07@gmail.com>
2020-10-14 10:56:55 -0300

    Add files via upload

---
+++ HiSolictionConsole/Hi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiSolictionConsole
+{
+    public class Hi
+    {
+        public double formulaMagica(double custo, int validade)
+        {
+            double preco = 0;
+            //CALCULO DE PREÇO A SER FEITO
+
+            return preco;
+        }
+    }
+}
---
+++ HiSolictionConsole/Hipermercado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiSolictionConsole
+{
+    public class Hipermercado
+    {
+        public Hi Hi = new Hi();        
+    }
+}
